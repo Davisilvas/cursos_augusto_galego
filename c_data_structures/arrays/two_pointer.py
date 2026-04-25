0516658e86ec3d2a5e86a152ed15aaf98c203c46
DavisilvaS <morg.daviss@gmail.com>
2026-04-25 19:48:59 -0300

    feat: added resolution with two point approach

---
+++ c_data_structures/arrays/two_pointer.py
@@ -0,0 +1,16 @@
+class solution:
+    def reverse_words_manual(self, s):
+        res = ' '
+        l, r = 0, 0
+
+        while r < len(s):
+            if s[r] != ' ':
+                r += 1
+            else:
+                res += s[l:r+1][::-1]
+                r += 1
+                l = r
+        
+        res += ' '
+        res += s[l:r +2][::-1]
+        return res [1:]
